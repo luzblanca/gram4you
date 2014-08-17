47948ba0e38dcec9b44cca75958f3dac94281c21
digitalman <dmitry.klimoff@gmail.com>
2014-08-18 00:47:08 +0400

    no message

--- js/script.js
+++ js/script.js
@@ -4,17 +4,18 @@ $(document).ready(function() {
   $('.form_phone').watermark("телефон");
   $('.form_phone').mask("+7 (999) 999-9999");
   $("form").on("submit", function(event) {
-    event.preventDefault();    
-    var form = this;
+    event.preventDefault();
+    var form = $(this);
 
-    $.post("send.php", $(form).serialize(),  function(response) {
+    $.post("send.php", form.serialize(),  function(response) {
+      
       // clear form
-      $(form).find("input[name=name]").val("");
-      $(form).find("input[name=phone]").val("");
+      form.find("input[name=name]").val("");
+      form.find("input[name=phone]").val("");
+
       // show result
-      $('#post_result_text').html(response);
-      $('#post_result').show().fadeTo('slow', 0.5).fadeTo('slow', 1.0);
-      $('html, body').animate({ scrollTop: $("#post_result").offset().top }, 400);
+      form.find(".post_result").html(response);
+      form.find(".post_result").show().fadeTo('slow', 0.5).fadeTo('slow', 1.0);
     });
   });
 });
